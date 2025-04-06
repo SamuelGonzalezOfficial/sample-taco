996711d243515698d23fa27da5d8bdf51d955ec7
Samuel <gonzalez.samuelalejandro@outlook.com>
2025-04-06 15:30:52 -0400

    We finish the Contact section.

--- src/features/Contact/Contact.jsx
+++ src/features/Contact/Contact.jsx
@@ -5,8 +5,8 @@ function Contact() {
     <section class="contact-container">
       <div class="contact-content">
         <div class="contact-image">
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15007931.799567098!2d-111.65577484275282!3d23.28414753193624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84043a3b88685353%3A0xed64b4be6b099811!2zTcOpeGljbw!5e0!3m2!1ses-419!2smx!4v1718401479097!5m2!1ses-419!2smx" 
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15007931.799567098!2d-111.65577484275282!3d23.28414753193624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84043a3b88685353%3A0xed64b4be6b099811!2zTcOpeGljbw!5e0!3m2!1ses-419!2smx!4v1718401479097!5m2!1ses-419!2smx"
             class="contact-img"
             allowFullScreen
             loading="lazy"
@@ -16,10 +16,13 @@ function Contact() {
         </div>
         <div class="contact-box">
           <div class="contact-header">
-            <h2 class="contact-title">Get in Touch</h2>
+            <h2 class="contact-title">
+              Dejanos tus comentarios
+            </h2>
             <p class="contact-subtitle">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-              eius.
+              Déjanos tu mensaje o consulta y te responderemos lo antes posible.
+              Ya sea para pedidos, reservas o eventos, estamos aquí para
+              ayudarte.
             </p>
           </div>
 
